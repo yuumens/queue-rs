3a8e6ed379728cef5da2aee2ca37c2246136f9f9
yuumens <ahmadrizki0209@gmail.com>
2026-06-17 12:40:39 +0700

    change faker init

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => \Faker\Factory::create()->name,
+            'name' => $this->faker->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),

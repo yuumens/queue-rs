358bd95e76a17858468f3a0a10fa2e74dfd7ec92
yuumens <ahmadrizki0209@gmail.com>
2026-06-17 13:27:25 +0700

    revert userfactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
